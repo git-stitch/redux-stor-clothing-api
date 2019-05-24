5015dbb8ee2667bc2d1bdbfc24985985ec2c9fb1
Brenden Williams <brendenw7@gmail.com>
2019-05-24 17:34:41 -0400

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+Product.create(name:"Pewdiepie Jean Jacket",categorie:"jacket",price:472,size:"s",url:"https://cdn-images.farfetch-contents.com/13/67/95/72/13679572_17913661_480.jpg")
+
+Product.create(name:"Supreme Camo",categorie:"shirt",price:300,size:"m",url:"https://cdn-images.farfetch-contents.com/13/84/86/72/13848672_17518862_480.jpg")
+
+Product.create(name:"Moschino Dotted Shorts",categorie:"shorts",price:176,size:"l",url:"https://cdn-images.farfetch-contents.com/13/32/07/61/13320761_16244671_480.jpg")
+
+
